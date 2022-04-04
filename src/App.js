5c136db464f199d12b5c61953ea03b8d14645f87
Kovács Attila <giga05091@hotmail.com>
2022-04-04 20:09:09 +0200

    few changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import Coin from "./Coin";
 import './App.css'
+import loadinggif from './loading.gif'
 function App() {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
+  const [more,setMore] = useState(10)
+  const [loading,setLoading] = useState(true)
 
   const handleChange = (e) => {
     setSearch(e.target.value);
@@ -16,36 +19,43 @@ function App() {
     coin.symbol.toLowerCase().includes(search.toLowerCase())
   );
 
+  const loadMore = () => {
+    setMore(more => more +10)
+  }
+ 
   useEffect(() => {
     Axios.get(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=huf&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-    ).then((res) => setCoins(res.data));
-  }, []);
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=huf&order=market_cap_desc&per_page=${more}&page=1&sparkline=false`
+    ).then((res) => setCoins(res.data)) && setLoading(false);
+  }, [more]);
 
   return (
-    <div className="coin-app">
-      <div className="coin-search">
-        <form>
-          <input
-            type="text"
-            placeholder="Search for a currency"
-            onChange={handleChange}
-          />
-        </form>
-      </div>
-      {filteredCoins.map((coin) => {
-        return (
-          <Coin
-            key={coin.id}
-            image={coin.image}
-            name={coin.name}
-            price={coin.current_price}
-            symbol={coin.symbol}
-            priceChange={coin.price_change_percentage_24h}
-          />
-        );
-      })}
+    loading ?  <img src={loadinggif} /> : <div className="coin-app">
+    <div className="coin-search">
+      <form>
+        <input
+          type="text"
+          placeholder="Search for a currency"
+          onChange={handleChange}
+        />
+      </form>
     </div>
+    {filteredCoins.map((coin) => {
+      return (
+        <Coin
+          key={coin.id}
+          image={coin.image}
+          name={coin.name}
+          price={coin.current_price}
+          symbol={coin.symbol}
+          priceChange={coin.price_change_percentage_24h}
+        />
+      );
+    })}
+    <div className="loadmore-button">
+    <button onClick={loadMore}>Load More</button>
+    </div>
+  </div>
   );
 }
 
